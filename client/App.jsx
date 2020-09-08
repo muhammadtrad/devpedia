0e7eeb8cc142085f7dba3f2c308da07def2da2a7
Jack Crish <jcrish@gmail.com>
2020-09-08 18:15:17 -0400

    bootable

---
+++ client/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+  <div className="app">
+    <h1>Amazing Todo List</h1>
+    <ul><li> Great work team</li></ul>
+  </div>
+);
+
+export default App;
